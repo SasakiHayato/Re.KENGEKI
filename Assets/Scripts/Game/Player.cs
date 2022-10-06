360ecd32acb0aba7b891de7c2f4742a3eab71c41
SasakiHayato0605 <zuomusundou685@gmail.com>
2022-10-06 16:17:47 +0900

    Player.csの更新、カメラに対する移動制御の追加

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,20 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+/// <summary>
+/// プレイヤーの管理クラス
+/// </summary>
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float _moveSpeed = 1;
+
+    Rigidbody _rb;
     InputOperator _inputOperator;
+
+    readonly float Gravity = Physics.gravity.y; 
+    
+    void Awake()
+    {
+        _inputOperator = new InputOperator();
+        _inputOperator.Enable();
+    }
 
     void Start()
     {
-        _inputOperator = new InputOperator();
-        
-
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        Move(_inputOperator.Player.Move.ReadValue<Vector2>());
+    }
+
+    void OnDestroy()
+    {
+        _inputOperator.Disable();
+    }
+
+    void Move(Vector2 dir)
+    {
+        Vector3 move;
+
+        if (CameraController.Data != null)
+        {
+            Vector3 forward = CameraController.Data.Foward * dir.y;
+            Vector3 right = CameraController.Data.Right * dir.x;
+
+            move = (forward + right) * _moveSpeed;
+        }
+        else
+        {
+            move = new Vector3(dir.x, 0, dir.y) * _moveSpeed;
+        }
+
+        move.y = Gravity;
+        _rb.velocity = move;
     }
 }
